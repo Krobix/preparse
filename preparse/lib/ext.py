bd85091e9baf50482826c345a5e0a17ffa08e0b6
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-16 16:59:09 -0500

    Delete ext.py

--- preparse/lib/ext.py
+++
@@ -1,8 +0,0 @@
-extensions = {}
-
-def def_ext(func, name):
-	'''
-	Allows you to define external commands. func parameter represents the actual function (Do not include the parenthesis), and name represents how it will be called.
-	'''
-	extensions[name] = func
-	
