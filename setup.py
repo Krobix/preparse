b491281575e9c395c8f8bbb34894c6c8fe227c52
Krobix <45689440+Krobix@users.noreply.github.com>
2019-02-21 12:05:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="preparse",
+	author="Krobix897",
+	author_email="osbrendon@gmail.com",
+	version="0.1.3",
+	description="A preprocessor for python",
+	packages=["preparse", "preparse.lib"])
